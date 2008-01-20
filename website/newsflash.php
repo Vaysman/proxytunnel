37a38f1a4ecff0cd981a591f87fcd16586e1f425
markjanssen <markjanssen@bc163920-b10d-0410-b2c5-a5491ca2ceef>
2008-01-20 13:09:22 +0000

    Update newsflash

--- website/newsflash.php
+++ website/newsflash.php
@@ -1,7 +1,7 @@
 <div class="rightside">
     <h3>Newsflash!</h3
-    <h4>Proxytunnel 1.7.0 released</h4>
-    <p>Proxytunnel 1.7.0 has been released. New features include reworked proctitle, signal handling, debugging output and more crypto options. The download is available on sourceforge.</p>
-    <p class="info"><HR>Added 2007/02/25 by Maniac</p>
+    <h4>Proxytunnel 1.8.1 soon</h4>
+    <p>Proxytunnel 1.8.1 will be released around the Fosdem 2008 conference. There are quite a few fixes in subversion and it's now possible to do basic-auth on the remote proxy as well.</p>
+    <p class="info"><HR>Added 2008/01/20 by Maniac</p>
  </div>
 
