bda6f84911f64b5c3a8ef786b6cf448b9f6c3389
dag- <dag-@bc163920-b10d-0410-b2c5-a5491ca2ceef>
2008-01-21 14:32:14 +0000

    Added newer binaries and subversion links.

--- website/download.php
+++ website/download.php
@@ -2,37 +2,49 @@
 
 <h2 class="headline">Getting ProxyTunnel</h2>
 
-<h3 CLASS="headline">Downloading source packages</h3>
-<UL>
-<LI><A HREF="http://sourceforge.net/project/showfiles.php?group_id=39840&package_id=32673&release_id=565105">proxytunnel-1.8.0.tgz</A>
-<LI><A HREF="http://sourceforge.net/project/showfiles.php?group_id=39840&package_id=32673&release_id=528619">proxytunnel-1.7.2.tgz</A>
-<LI><A HREF="http://sourceforge.net/project/showfiles.php?group_id=39840&package_id=32673&release_id=489199">proxytunnel-1.7.0.tgz</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.3.tgz?download">proxytunnel-1.6.3.tgz</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.3.tgz.asc?download">proxytunnel-1.6.3.tgz.asc</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.0.tgz?download">proxytunnel-1.6.0.tgz</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.0.tgz.asc?download">proxytunnel-1.6.0.tgz.asc</A> (pgp/gpg signature)
-</UL></P>
-
 <h3 CLASS="headline">From the sourceforge download site</h3>
 <P>To download our releases from the sourceforge download site, please visit <A HREF="http://sourceforge.net/project/showfiles.php?group_id=39840" TARGET='main'>sourceforge at http://sourceforge.net/project/showfiles.php?group_id=39840</A></p>
+
+<h3 CLASS="headline">Downloading source packages</h3>
+<P><UL>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.8.0.tgz?download">proxytunnel-1.8.0.tgz</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.8.0.tgz.asc?download">proxytunnel-1.8.0.tgz.asc</A> (pgp/gpg signature)
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.7.2.tgz?download">proxytunnel-1.7.2.tgz</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.7.2.tgz.asc?download">proxytunnel-1.7.2.tgz.asc</A> (pgp/gpg signature)
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.7.0.tgz?download">proxytunnel-1.7.0.tgz</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.7.0.tgz.asc?download">proxytunnel-1.7.0.tgz.asc</A> (pgp/gpg signature)
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.3.tgz?download">proxytunnel-1.6.3.tgz</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.3.tgz.asc?download">proxytunnel-1.6.3.tgz.asc</A> (pgp/gpg signature)
+</UL></P>
+
+<h3 CLASS="headline">From Subversion</h3>
+<P>Check out the ProxyTunnel sourcecode from SourceForge's SVN server using the following command:
+<BLOCKQUOTE>
+svn co <A HREF="https://proxytunnel.svn.sourceforge.net/svnroot/proxytunnel/">https://proxytunnel.svn.sourceforge.net/svnroot/proxytunnel/</A> proxytunnel
+</BLOCKQUOTE>
+or browse the ProxyTunnel subversion tree at:
+<UL>
+<A HREF="http://proxytunnel.svn.sourceforge.net/viewvc/proxytunnel/">http://proxytunnel.svn.sourceforge.net/viewvc/proxytunnel/</A>
+</UL></P>
+
+<h3 CLASS="headline">Downloading RPM packages</h3>
+<P>The following packages are made for Red Hat Enterprise Linux and CentOS.
+<UL>
+<LI><A HREF="http://dag.wieers.com/rpm/packages/proxytunnel/">http://dag.wieers.com/rpm/packages/proxytunnel/</A> (external RPM packages)
+</UL></P>
 
 <h3 CLASS="headline">Precompiled binaries for your platform</h3>
 <P>This list contains binaries created on the sourceforge compile farm. They may be older releases (just proxytunnel binary, nothing else). If at all possible, compile your own.
 <UL>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.2-rc-cygwin.zip?download">Proxytunnel-1.6.2-rc-cygwin.zip</A>, version 1.6.2-rc for windows, includes required cygwin libraries, linked with ssl.
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-win32.zip?download">Proxytunnel-win32.zip</A>, version 1.6.0-rc1 for windows, includes required cygwin libraries
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-linux?download">Proxytunnel 1.6.0-rc1 linux-x86 binary</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-netbsd?download">Proxytunnel 1.6.0-rc1 netbsd-x86 binary</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-openbsd?download">Proxytunnel 1.6.0-rc1 openbsd-x86 binary</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-solaris?download">Proxytunnel 1.6.0-rc1 solaris-x86 binary</A>
-<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-sparc-solaris?download">Proxytunnel 1.6.0-rc1 solaris-sparc binary</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel.exe?download">proxytunnel.exe</A> (version 1.8.0 for windows, only executable)
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.7.0-win32.zip?download">proxytunnel-1.7.0-win32.zip</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-1.6.2-rc-cygwin.zip?download">proxytunnel-1.6.2-rc-cygwin.zip</A>, version 1.6.2-rc for windows, includes required cygwin libraries, linked with ssl.
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-win32.zip?download">proxytunnel-win32.zip</A>, version 1.6.0-rc1 for windows, includes required cygwin libraries
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-linux?download">proxytunnel 1.6.0-rc1 linux-x86 binary</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-netbsd?download">proxytunnel 1.6.0-rc1 netbsd-x86 binary</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-openbsd?download">proxytunnel 1.6.0-rc1 openbsd-x86 binary</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-x86-solaris?download">proxytunnel 1.6.0-rc1 solaris-x86 binary</A>
+<LI><A HREF="http://prdownloads.sourceforge.net/proxytunnel/proxytunnel-sparc-solaris?download">proxytunnel 1.6.0-rc1 solaris-sparc binary</A>
 </UL></P>
-
-<h3 CLASS="headline">From CVS</h3>
-<P>Check out the ProxyTunnel sourcecode from SourceForge's CVS server using the following command:
-<BLOCKQUOTE>
-cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/proxytunnel co proxytunnel
-</BLOCKQUOTE>
-or download the latest daily CVS snapshot on the following url: <A HREF="http://cvs.sourceforge.net/cvstarballs/proxytunnel-cvsroot.tar.bz2" TARGET='main'>http://cvs.sourceforge.net/cvstarballs/proxytunnel-cvsroot.tar.bz2</A>
 
 <?php include("_end.php"); ?>
